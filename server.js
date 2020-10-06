7c666a8ef4b85f5f11dbc0da2ec761e05a9ee76f
chamodya wimansha <chamodyawimansha@gmail.com>
2020-10-06 20:10:51 +0530

    new user register works

--- server.js
+++ server.js
@@ -2,11 +2,37 @@ var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 var app = express();
 
 const port = process.env.PORT;
 app.use(cors());
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+});
+
+const ExerciseSchema = new mongoose.Schema({
+  userId: String,
+  description: String,
+  duration: String,
+  date: Date,
+});
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  return res.json({ error: "Database Error" });
+});
+
+const User = mongoose.model("User", UserSchema);
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
 /** this project needs to parse POST bodies **/
 app.use(bodyParser.json()); // to support JSON-encoded bodies
@@ -23,7 +49,25 @@ app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post("/api/exercise/new-user", (req, res) => {});
+app.post("/api/exercise/new-user", (req, res) => {
+  let username = req.body.username;
+
+  if (username.replace(/ /g, ";") == "") {
+    return res.json({ error: "username is required" });
+  }
+
+  let newUser = new User({
+    username: username,
+  });
+
+  newUser.save((error) => {
+    if (error) {
+      return res.json({ error: "Database Error" });
+    } else {
+      return res.json({ username: newUser.username, _id: newUser.id });
+    }
+  });
+});
 
 app.post("/api/exercise/log/:userId/:from?/:to?/:limit?", (req, res) => {});
 
